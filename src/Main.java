25e678316ecfb1a1569ddde686359f87f34d6bf9
pt-kt <tany162@mail.ru>
2025-06-01 15:26:59 +0300

    1/3 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -7,35 +7,53 @@ public class Main {
         }
     }
 
-        public static String installingOsAndYear(int clientOs, int clientDeviceYear) {
-            if (clientOs == 1) {
-                if (clientDeviceYear <= 2015) {
-                    return "Установите облегченную версию приложения для Android по ссылке";
-                } else {
-                    return "Установите версию приложения для Android по ссылке";
-                }
+    public static String installingOsAndYear(int clientOs, int clientDeviceYear) {
+        if (clientOs == 1) {
+            if (clientDeviceYear < 2015) {
+                return "Установите облегченную версию приложения для Android по ссылке";
             } else {
-                if (clientDeviceYear <= 2015) {
-                    return "Установите облегченную версию приложения для iOS по ссылке";
-                } else {
-                    return "Установите версию приложения для iOS по ссылке";
-                }
+                return "Установите версию приложения для Android по ссылке";
+            }
+        } else if (clientOs == 0) {
+            if (clientDeviceYear < 2015) {
+                return "Установите облегченную версию приложения для iOS по ссылке";
+            } else {
+                return "Установите версию приложения для iOS по ссылке";
             }
         }
+        return "Такой ОC не существует";
+    }
 
-        public static void main (String[] args) {
+    public static String calculatingDeliveryDays(int deliveryDistance, int timeOneZoneDelivery) {
+        if (timeOneZoneDelivery == 1) {
+        if (deliveryDistance > 100) {
+            return "Доставки нет";
+        } else if (deliveryDistance > 60) {
+            timeOneZoneDelivery += 2;
+        } else if (deliveryDistance > 20) {
+            timeOneZoneDelivery += 1;
+        }
+        return "Потребуется дней доставки:  " + timeOneZoneDelivery;
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
 
             System.out.println("Задача 1");
             calculateLeapYear(2024);
 
             System.out.println("Задача 2");
-            installingOsAndYear (1, 2026);
+            System.out.println(installingOsAndYear(1, 2014));
 
-
+            System.out.println("Задача 3");
+            System.out.println(calculatingDeliveryDays(1, 35));
 
         }
-
     }
+
+
+
 
 
 

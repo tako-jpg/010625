bd7dc97f0b7dce37c09397c18ea4d6b21cf4572f
pt-kt <tany162@mail.ru>
2025-06-01 12:19:43 +0300

    1/3 ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void calculateLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год — високосный год");
+        } else {
+            System.out.println(year + " год — невисокосный год");
+        }
     }
-}
+
+        public static String installingOsAndYear(int clientOs, int clientDeviceYear) {
+            if (clientOs == 1) {
+                if (clientDeviceYear <= 2015) {
+                    return "Установите облегченную версию приложения для Android по ссылке";
+                } else {
+                    return "Установите версию приложения для Android по ссылке";
+                }
+            } else {
+                if (clientDeviceYear <= 2015) {
+                    return "Установите облегченную версию приложения для iOS по ссылке";
+                } else {
+                    return "Установите версию приложения для iOS по ссылке";
+                }
+            }
+        }
+
+        public static void main (String[] args) {
+
+            System.out.println("Задача 1");
+            calculateLeapYear(2024);
+
+            System.out.println("Задача 2");
+            installingOsAndYear (1, 2026);
+
+
+
+        }
+
+    }
+
+
+

a9de5d7dde49655b1eaffc9c068047964ccad83f
pt-kt <tany162@mail.ru>
2025-06-03 05:26:13 +0300

    попытка 3 сдать ДЗ

--- src/Main.java
+++ src/Main.java
@@ -9,49 +9,45 @@ public class Main {
         }
     }
 
-    public static String installingOsAndYear(int clientOs, int clientDeviceYear) {
-        clientOs = 0;
-        clientDeviceYear = 2016;
+    public static void installingOsAndYear(int clientOs, int clientDeviceYear) {
         if (clientOs == 0 && clientDeviceYear < 2025) {
-            System.out.println("Установите облегченную версию приложение для системы iOS по ссылке");
+            System.out.println("Установите облегченную версию приложения для системы iOS по ссылке");
         } else if (clientOs == 0) {
-            System.out.println("Установите приложение для системы iOS по ссылке");
+            System.out.println("Установите приложения для системы iOS по ссылке");
         }
         if (clientOs == 1 && clientDeviceYear < 2025) {
-            System.out.println("Установите облегченную версию приложение для системы Android по ссылке");
+            System.out.println("Установите облегченную версию приложения для системы Android по ссылке");
         } else if (clientOs == 1) {
-            System.out.println("Установите приложение для системы Android по ссылке");
-        }
-
-        public static String calculatingDeliveryDays(int deliveryDistance) {
-            int timeOneZoneDelivery = 1;
-            if (deliveryDistance > 100) {
-                return "Доставки нет";
-            } else if (deliveryDistance > 60) {
-                timeOneZoneDelivery += 2;
-            } else if (deliveryDistance > 20) {
-                timeOneZoneDelivery += 1;
-            }
-            return "Потребуется дней доставки: " + timeOneZoneDelivery;
-
-        }
-
-        public static void main(String[] args) {
-
-            System.out.println("Задача 1");
-            calculateLeapYear(2024);
-
-            System.out.println("Задача 2");
-            System.out.println(installingOsAndYear(1, 2014));
-
-            System.out.println("Задача 3");
-            System.out.println(calculatingDeliveryDays(40));
-
+            System.out.println("Установите приложения для системы Android по ссылке");
         }
     }
+
+    public static void calculatingDeliveryDays(int deliveryDistance) {
+        int timeOneZoneDelivery = 1;
+        if (deliveryDistance > 100) {
+            System.out.println("Доставки нет");
+        } else if (deliveryDistance > 60) {
+            timeOneZoneDelivery += 2;
+        } else if (deliveryDistance > 20) {
+            timeOneZoneDelivery += 1;
+        }
+        if (deliveryDistance <= 100) { // Убедись, что вывод происходит вне условий
+            System.out.println("Потребуется дней доставки: " + timeOneZoneDelivery);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+        calculateLeapYear(2024);
+
+        System.out.println("Задача 2");
+        installingOsAndYear(1, 2014);
+
+        System.out.println("Задача 3");
+        calculatingDeliveryDays(110);
+    }
 }
-
-
 
 
 

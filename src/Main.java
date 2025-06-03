63badfbd0e49a36517da35833de7223482423d85
pt-kt <tany162@mail.ru>
2025-06-03 16:39:02 +0300

    попытка 4 сдать ДЗ

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ public class Main {
         }
     }
 
-    public static void installingOsAndYear(int clientOs, int clientDeviceYear) {
+    public static void installingOsAndYear(int clientOs) {
+        int clientDeviceYear = 2025;
         if (clientOs == 0 && clientDeviceYear < 2025) {
             System.out.println("Установите облегченную версию приложения для системы iOS по ссылке");
         } else if (clientOs == 0) {
@@ -22,18 +23,16 @@ public class Main {
         }
     }
 
-    public static void calculatingDeliveryDays(int deliveryDistance) {
-        int timeOneZoneDelivery = 1;
+    public static String calculatingDeliveryDays(int deliveryDistance) {
+        int totalDays = 1;
         if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
+            return "Доставки нет";
         } else if (deliveryDistance > 60) {
-            timeOneZoneDelivery += 2;
+            totalDays += 2;
         } else if (deliveryDistance > 20) {
-            timeOneZoneDelivery += 1;
+            totalDays += 1;
         }
-        if (deliveryDistance <= 100) { // Убедись, что вывод происходит вне условий
-            System.out.println("Потребуется дней доставки: " + timeOneZoneDelivery);
-        }
+        return "Потребуется дней: " + totalDays;
     }
 
     public static void main(String[] args) {
@@ -42,10 +41,10 @@ public class Main {
         calculateLeapYear(2024);
 
         System.out.println("Задача 2");
-        installingOsAndYear(1, 2014);
+        installingOsAndYear(0);
 
         System.out.println("Задача 3");
-        calculatingDeliveryDays(110);
+        System.out.println(calculatingDeliveryDays (40));
     }
 }
 
